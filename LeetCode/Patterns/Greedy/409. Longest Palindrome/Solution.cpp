48cf84f5c015f3a0ea512f6b48cb1d1998551acf
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-08-15 16:42:47 +0530

    Add C++ Solution to Patterns/Greedy

---
+++ LeetCode/Patterns/Greedy/409. Longest Palindrome/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        int count=1;
+        int m=0;
+        int sum=0;
+        for(auto x: mp){
+            if(x.second % 2 == 0){
+                sum += x.second;
+            }
+            if(x.second % 2 != 0){
+                if(m < x.second){
+                    m= x.second;
+                }
+            }
+        }
+        sum += m;
+        return sum;
+    }
+};
