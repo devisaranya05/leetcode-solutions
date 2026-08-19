e965a231bc3cb75b314c32a26f468151a3cebcac
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-08-19 15:26:44 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/2124. Check if All As Appears Before All Bs/Solution.cpp
+++ LeetCode/C++/Easy/2124. Check if All As Appears Before All Bs/Solution.cpp
@@ -3,8 +3,10 @@ public:
     bool checkString(string s) {
         int c=0;
         for(int i=0; i<s.size()-1; i++){
-           if(s[i] != s[i+1]){
-            cout << c << " ";
+            if((s[i] == 'b' && s[i+1]=='a')){
+                return false;
+            }
+           if((s[i] != s[i+1])){
             c++;
            }
         }
