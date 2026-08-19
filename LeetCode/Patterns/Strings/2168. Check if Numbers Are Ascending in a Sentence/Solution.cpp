0e74eabae3bd53cba68130e7b1a2ffced389dcd5
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-08-19 15:05:03 +0530

    Add C++ Solution to Patterns/Strings

---
+++ LeetCode/Patterns/Strings/2168. Check if Numbers Are Ascending in a Sentence/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        int n;
+        int m=0;
+        bool r=true;
+        for(int i=0; i<s.size(); i++){
+              if((s[i] >='A' && s[i] <='Z') || (s[i]>='a' && s[i]<='z') || s[i]==' '){
+
+              }
+              else{
+                   if((s[i+1] >='A' && s[i+1] <='Z') || (s[i+1]>='a' && s[i+1]<='z') || s[i+1]==' '){
+                       n=s[i]-'0';
+                       if(m >= n){
+                          return false;
+                       }
+                       else{
+                        m=n;
+                       }
+                   }
+                   else{
+                      n=(s[i]-'0')*10;
+                      n += (s[i+1]-'0');
+                      if(m >= n){
+                          return false;
+                       }
+                       else{
+                        m=n;
+                       }
+                       i++;
+                   }
+              }
+        }
+        return r;
+    }
+};
