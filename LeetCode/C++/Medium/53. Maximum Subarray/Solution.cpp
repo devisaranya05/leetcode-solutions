c80dbd09bd92d1703d91fc0a5a0bc36d83283ed2
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-08-25 11:57:40 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/53. Maximum Subarray/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int cs = 0;
+       int ms = nums[0];
+       for(int i=0; i<nums.size(); i++){
+        cs += nums[i];
+        if(cs > ms){
+            ms = cs;
+        }
+        if(cs < 0){
+            cs=0;
+        }
+       } 
+       return ms;
+    }
+};
