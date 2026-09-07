fec09133fa4223aa76b2118099fc77ac8945422f
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-09-07 19:50:21 +0530

    Add Medium C++ Solution for 167. Two Sum II - Input Array Is Sorted from LeetCode

--- LeetCode/C++/Medium/167. Two Sum II - Input Array Is Sorted/Solution.cpp
+++ LeetCode/C++/Medium/167. Two Sum II - Input Array Is Sorted/Solution.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        bool f=true;
-        int l=0;
-        int r=1;
-        vector<int>p;
-        while(f){
-           if(numbers[l]+numbers[r]==target){
-             f=false;
-             p.push_back(l+1);
-             p.push_back(r+1);
-             break;
-           }
-           else if(numbers[l]+numbers[r] < target){
-             r++;
-           }
-           else{
-            l--;
-           }
+        // bool f=true;
+        // int l=0;
+        // int r=1;
+        // vector<int>p;
+        // while(f){
+        //    if(numbers[l]+numbers[r]==target){
+        //      f=false;
+        //      p.push_back(l+1);
+        //      p.push_back(r+1);
+        //      break;
+        //    }
+        //    else if(numbers[l]+numbers[r] < target){
+        //      r++;
+        //    }
+        //    else{
+        //     l--;
+        //    }
+        // }
+        // return p;
+
+
+
+
+
+         int l = 0;
+        int r = numbers.size() - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+
+            if (sum == target) {
+                return {l + 1, r + 1};
+            }
+            else if (sum < target) {
+                l++;
+            }
+            else {
+                r--;
+            }
         }
-        return p;
+
+        return {};
+
+
+
     }
 };
