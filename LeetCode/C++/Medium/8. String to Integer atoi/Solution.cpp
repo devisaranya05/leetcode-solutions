4ae898cc4378486ffa75c81ab5643f0e00709122
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-09-17 20:21:42 +0530

    Add Medium C++ Solution for 8. String to Integer (atoi) from LeetCode

--- LeetCode/C++/Medium/8. String to Integer atoi/Solution.cpp
+++ LeetCode/C++/Medium/8. String to Integer atoi/Solution.cpp
@@ -1,34 +1,6 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long result = 0;
-        int sign = 1;
-        int i = 0;
-
-        
-        while (i < s.length() && s[i] == ' ') {
-            i++;
-        }
-        if (i < s.length() && (s[i] == '-' || s[i] == '+')) {
-            sign = (s[i] == '-') ? -1 : 1;
-            i++;
-        }
-
-        
-        while (i < s.length() && isdigit(s[i])) {
-            result = result * 10 + (s[i] - '0');
-            
-            
-            if (sign == 1 && result > std::numeric_limits<int>::max()) {
-                return std::numeric_limits<int>::max();
-            }
-            if (sign == -1 && -result < std::numeric_limits<int>::min()) {
-                return std::numeric_limits<int>::min();
-            }
-            i++;
-        }
-
-        return static_cast<int>(result * sign);
-
+        return stoi(s);
     }
 };
--- LeetCode/Patterns/Strings/8. String to Integer atoi/README.md
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/README.md
@@ -8,8 +8,8 @@
 String
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 9.2 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
